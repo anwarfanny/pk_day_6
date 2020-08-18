10b07f457b2c76fe0d675d2cddf2e9707c0959e2
fanni <anwarainul>
2020-08-18 22:45:20 +0700

    pk_day_6

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ $("p").click(function () {
+     alert("it's Clicked")
+     // $(this).hide()
+     // $(this).hide("slow", function () {
+     //     alert("it's gone now")
+     //})
+     $(this).hide
+ })
+})
